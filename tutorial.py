97f0e9a6dd960f6e48a8798571538cf113d3f8b9
Qiaoying C <qchen53@buffalo.edu>
2023-09-20 19:26:29 -0400

    combine tutorial.py (game window) and mouse_movement.py (move player by mouse)

--- tutorial.py
+++ tutorial.py
@@ -7,19 +7,24 @@ from os.path import isfile, join
 
 pygame.init()
 
-pygame.display.set_caption("PlayerMap")
+# caption and icon
+pygame.display.set_caption("Survivor")
+# icon = pygame.image.load("FILE_NAME")
+# pygame.display.set_icon(icon)
 
-MAP_COLOR = (127, 127, 127) # 地图背景颜色
-WIDTH, HEIGHT = 1000, 800 # 游戏窗口尺寸
+MAP_COLOR = (127, 127, 127) # map background color 地图背景颜色
+WIDTH, HEIGHT = 1000, 800 # window size 游戏窗口尺寸
+WHITE = (255, 255, 255)
+bg_img_name = "地砖.png"
 
 FPS = 60 # 每秒60帧
-PLAYER_VEL = 5 #玩家移动速度
+PLAYER_VEL = 3 #player speed 玩家移动速度
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def get_background(name):
-    image = pygame.image.load(join("assets","background",name))
-    width, height = image.get_width(), image.get_height()
+def get_background():
+    background_img = pygame.image.load(join("asset","background",bg_img_name))
+    width, height = background_img.get_width(), background_img.get_height()
     tiles = []
 
     for i in range(WIDTH // width + 1):
@@ -27,29 +32,62 @@ def get_background(name):
             pos = (i * width, j * height)
             tiles.append(pos)
     
-    return tiles, image
+    return tiles, background_img
 
 def draw(window, background, bg_image):
     for tile in background:
         window.blit(bg_image, tile)
 
-    pygame.display.update()
+# player
+# playerIMG = pygame.image.load('FILE_NAME')
+playerIMG = pygame.Rect(50, 50, 30, 30) # use rectangle for now
+playerX = 500
+playerY = 400
 
+# def player():
+#     window.blit(playerIMG,(playerX,playerY))
+    
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image  = get_background("地砖.png")
+    tiles, bg_image  = get_background()
 
-    run = True
-    while run:
+    # Variables for tracking player movement
+    moving = False
+    target_x, target_y = playerIMG.centerx, playerIMG.centery
+
+    running = True
+    while running:
         clock.tick(FPS)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-                break
+                running = False
+            # check if mouse click
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Left mouse button clicked
+                    target_x, target_y = pygame.mouse.get_pos()
+                    moving = True
 
-        draw(window, background, bg_image)
-    
+        # Calculate the direction vector from player to target
+        dx, dy = target_x - playerIMG.centerx, target_y - playerIMG.centery
+        distance = math.sqrt(dx ** 2 + dy ** 2)
+
+        if moving and distance > 0:
+            # Normalize the direction vector
+            dx /= distance
+            dy /= distance
+
+            # Move the player in the direction
+            playerIMG.x += dx * PLAYER_VEL
+            playerIMG.y += dy * PLAYER_VEL
+   
+        draw(window, tiles, bg_image) # upload background
+        
+        # Draw the character
+        pygame.draw.rect(window, (0, 0, 255), playerIMG)
+
+        pygame.display.update()
+        
     pygame.quit()
     quit()
 
