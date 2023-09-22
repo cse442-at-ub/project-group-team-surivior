16dc6a0f86f680fa1d311f16b4bfd7d123423ea0
yluoc <98978059+yluoc@users.noreply.github.com>
2023-09-21 21:31:28 -0400

    items list

---
+++ items.py
@@ -0,0 +1,95 @@
+#items
+ITEMS = {}
+#currency
+ITEMS['gold'] = {'value': 1} # need add image
+
+#shoes
+ITEMS["Berserker's Greaves"] = {'attack speed': 0.35, 'movement speed': 45}
+ITEMS['Ionian Boots of Lucidity'] = {'ability haste': 20, 'movement': 45}
+ITEMS["Mercury's Treads"] = {'magic resistance': 25, 'movement': 45}
+ITEMS['Plated Steelcaps'] = {'armor': 20,'movement': 45}
+ITEMS["Sorcerer's Shoes"] = {'magic penetration': 18, 'movement': 45}
+
+#start items
+ITEMS['Amplifying Tome'] = {'ability power': 20}
+ITEMS['Agility Cloak'] = {'critical strike chance': 0.15}
+ITEMS['Cloth Armor'] = {'armor': 15}
+ITEMS['Magic Mantle'] = {'magic resistance': 25}
+ITEMS['Dagger'] = {'attack speed': 0.12}
+ITEMS['Long Sword'] = {'attack damage': 10}
+ITEMS['Ruby Crystal'] = {'health': 150}
+ITEMS['Sapphire Crystal'] = {'mana': 250}
+
+#Epic items
+ITEMS['Serrated Dirk'] = {'attack damage': 30, 'lethality': 10}
+ITEMS['Tiamat'] = {'attack damage': 25} #UNIQUE
+ITEMS['Warhammer'] = {'attack damage': 25, 'ability haste': 10}
+ITEMS['Hexdrinker'] = {'attack damage': 25, 'magic resistance': 35}
+ITEMS['Phage'] = {'attack damage': 15, 'health': 200}
+ITEMS['Sheen'] = {}#UNIQUE
+ITEMS['Kindlegem'] = {'ability haste': 10, 'health': 200}
+ITEMS['ampiric Scepter'] = {'attack damage': 10, 'lifesteal': 0.07}
+ITEMS['Fiendish Codex'] = {'ability power': 35, 'aility haste': 10}
+ITEMS['Hextexh Alternator'] = {'ability power': 25, 'health': 150}#UNIQUE
+ITEMS['Lost Chapter'] = {'ability power': 40, 'ability haste': 10,'mana': 300}
+ITEMS['Catalyst of Aeons'] = {'health': 300, 'mana': 300}#UNIQUE
+ITEMS['Leeching Leer'] = {'ability power': 20, 'health': 250, 'omnivamp': 0.5}
+ITEMS['Needlessly Large Rod'] = {'ability power': 60}
+ITEMS['Hearthbound Axe'] = {'attack damage': 20, 'attack speed': 0.15}#UNIQUE
+ITEMS['Kircheis Shard'] = {'attack damage': 15}#UNIQUE
+ITEMS['Last Whisper'] = {'attack damage': 20, 'armor penetration': 0.18}
+ITEMS['Noonquiver'] = {'attack damage': 30, 'attak speed': 0.15}#UNIQUE
+ITEMS['Rageknife'] = {'attack speed': 0.25}#UNIQUE
+ITEMS['Recurve Bow'] = {'attack speed': 0.15}#UNIQUE
+ITEMS['Zeal'] = {'attack speed': 0.15, 'critical strike chance': 0.15, 'movement': 0.05}
+ITEMS['B.F.Sword'] = {'attack damage': 40}
+ITEMS["Bami's Cinder"] = {'health': 300}#UNIQUE
+ITEMS['Bramble Vest'] = {'Armor': 30}#UNIQUE
+ITEMS['Chain Vest'] = {'armor': 40}
+ITEMS["Giant's Belt"] = {'health': 350}
+ITEMS['Glacial Buckler'] = {'ability haste': 10, 'mana': 250, 'armor': 20}
+ITEMS['Negatron Cloak'] = {'magic resistance': 50}
+ITEMS["Spectre's Cowl"] = {'health': 250, 'magic resistance': 25}#UNIQUE
+ITEMS["Warden's Mail"] = {'armor': 40}#UNIQUE
+ITEMS['Winged Moonplate'] = {'health': 150}#UNIQUE
+ITEMS['Aegis of the Legion'] = {'ability haste': 10, 'armor': 30,'magic resistance': 30}
+
+#Legendary items
+ITEMS['Maw of Malmortius'] = {'attack damage': 65, 'magic resistance': 50}#UNIQUE
+ITEMS['Titanic Hydra'] = {'attack damage': 30, 'health': 500}#UNIQUE
+ITEMS['Ravenous Hydra'] = {'attack damage': 65, 'ability haste': 25, 'lifesteal': 0.1}#UNIQUE
+ITEMS["Death's Dance"] = {'attack damage': 55, 'ability haste': 15, 'armor': 45}#UNIQUE
+ITEMS['Spear of Shojin'] = {'attack damage': 55, 'ability haste': 20, 'health': 500}#UNIQUE
+ITEMS['Divine Sunderer'] = {'attack damage': 40, 'ability haste': 20, 'health': 300}#UNIQUE
+ITEMS['Cosmic Drive'] = {'ability power': 100, 'ability haste': 30, 'movement': 0.05}
+ITEMS['Demonic Embrace'] = {'ability power': 75, 'health': 350}#UNIQUE
+ITEMS['Horizon Focus'] = {'ability power': 100, 'ability haste': 15, 'health': 150}#UNIQUE
+ITEMS['Lich Bane'] = {'ability power': 85, 'ability haste': 15, 'movement': 0.08}#UNIQUE
+ITEMS["Rabadon's Deathcap"] = {'ability power': 120}#UNIQUE
+ITEMS["Rylai's Crystal Scepter"] = {'ability power': 75, 'health': 400}#UNIQUE
+ITEMS['Void Staff'] = {'ability power': 65, 'magic penetration': 0.4}
+ITEMS['Rod of Ages'] = {'ability power': 60, 'health': 400, 'mana':400}#UNIQUE
+ITEMS['Hextech Gunlade'] = {'ability power': 80, 'attack damage': 40, 'omnivamp': 0.2}#UNIQUE
+ITEMS["Luden's Tempest"] = {'ability power': 80, 'ability haste': 20, 'mana': 600, 'magic penetration': 6}#UNIQUE
+ITEMS["Liandry's Anguish"] = {'ability power': 100, 'ability haste': 20, 'mana': 600}#UNIQUE
+ITEMS['Bloodthirster'] = {'attack damage': 55, 'critical strike chance': 0.2, 'lifesteal': 0.18}#UNIQUE
+ITEMS['kraken Slayer'] = {'attack damage': 40, 'attack speed': 0.3, 'critical strike chance': 0.2}#UNIQUE
+ITEMS["Lord Dominik's Regards"] = {'attack damage': 35, 'critical strike chance': 0.2, 'armor penetration': 0.3}#UNIQUE
+ITEMS["Nashor's Tooth"] = {'ability power': 100, 'ability haste': 15, 'attack speed': 0.5}#UNIQUE
+ITEMS["Rapid Firecannon"] = {'attack damage': 30, 'attack speed': 0.15, 'critical strike chance': 0.2, 'movement': 0.07}#UNIQUE
+ITEMS["Runaan's Hurricane"] = {'attack speed': 0.4, 'critical strike chance': 0.2, 'movement': 0.07}#UNIQUE
+ITEMS['Statikk Shiv'] = {'attack damage': 50, 'critical strike chance': 0.2, 'attack speed': 0.3}#UNIQUE
+ITEMS["Wit's End"] = {'attack damage': 40, 'attack speed': 0.4, 'magic resistance': 40}#UNIQUE
+ITEMS['Navori Quickblades'] = {'attack damage': 65, 'ability haste': 15, 'critical strike chance': 0.2}#UNIQUE
+ITEMS["Guinsoo's Rageblade"] = {'attack damage': 30, 'attack speed': 0.25, 'ability power': 30}#UNIQUE
+ITEMS['Abyssal Mask'] = {'ability haste': 10, 'health': 300, 'magic resistance': 60}#UNIQUE
+ITEMS["Dead Man's Plate"] = {'armor': 45, 'health': 300, 'movement': 0.05}#UNIQUE
+ITEMS['Force of Nature'] = {'magic rresistance': 60, 'health': 400, 'movement': 0.05}#UNIQUE
+ITEMS["Randuin's Omen"] = {'health': 400, 'armor': 60}#UNIQUE
+ITEMS['Spirit Visage'] = {'ability haste': 10, 'health': 450, 'magic resistance': 60, 'health regen': 1}#UNIQUE / health regen: 100%
+ITEMS["Sterak's Gage"] = {'health': 450}#UNIQUE
+ITEMS['Thornmail'] = {'health': 350, 'armor': 70}#UNIQUE
+ITEMS["Warmog's Armor"] = {'ability haste': 10, 'health': 800, 'health regen': 2}#UNIQUE / health regen: 200%
+ITEMS['Frostfire Gauntlet'] = {'ability haste': 20, 'health': 450, 'magic resistance': 25, 'armor': 25}#UNIQUE
+ITEMS["Jak'sSho, The Protean"] = {'ability haste': 20, 'health': 400, 'magic resistance': 30, 'armor': 30}#UNIQUE
+ITEMS['Heartsteel'] = {'ability haste': 20, 'health': 800, 'health regen': 2}#UNIQUE / health regen: 200%
