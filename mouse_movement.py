dafcce0c3d31d532f6aafe9ad97fcb92cef49761
Qiaoying C <qchen53@buffalo.edu>
2023-10-16 19:30:25 -0400

    merge the newest mouse_movement.py

--- mouse_movement.py
+++ mouse_movement.py
@@ -5,18 +5,29 @@ def game():
     pygame.init()
 
     WIDTH, HEIGHT = 800, 600
-    CHARACTER_SPEED = 2
+    ENEMY_SPEED = 1
+    CHARACTER_SPEED = 2  
+    FPS = 60  
 
     WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    PINK = (255, 105, 180)  # RGB color code for pink
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Character Follows Mouse")
+    pygame.display.set_caption("Character Chases Red Square")
 
-    character = pygame.Rect(50, 50, 30, 30)
+    enemy = pygame.Rect(50, 50, 30, 30)  # blue square
+    character = pygame.Rect(200, 200, 30, 30)  # red square
 
-    # Variables for tracking character movement
     moving = False
-    target_x, target_y = character.centerx, character.centery
+    health = 20  # Character's initial health
+    damage = 10  # Initial damage dealt by the enemy
+    attack_timer = 0  # Timer for enemy attack
+    color_change_timer = 0  # Timer for character color change
+
+    clock = pygame.time.Clock() 
 
     running = True
     while running:
@@ -24,31 +35,53 @@ def game():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:  # Left mouse button clicked
-                    target_x, target_y = pygame.mouse.get_pos()
+                if event.button == 1:  
+                    character_x, character_y = pygame.mouse.get_pos()
                     moving = True
 
-        # Calculate the direction vector from character to target
-        dx, dy = target_x - character.centerx, target_y - character.centery
+        if moving:
+            dx, dy = character_x - character.centerx, character_y - character.centery
+            distance = math.sqrt(dx ** 2 + dy ** 2)
+
+            if distance > 0:
+                dx /= distance
+                dy /= distance
+
+                character.x += dx * CHARACTER_SPEED
+                character.y += dy * CHARACTER_SPEED
+
+        dx, dy = character.centerx - enemy.centerx, character.centery - enemy.centery
         distance = math.sqrt(dx ** 2 + dy ** 2)
 
-        if moving and distance > 0:
-            # Normalize the direction vector
+        if health > 0 and distance <= 15 and attack_timer <= 0:  
+            health -= damage  # Reduce character's health
+            attack_timer = FPS  # Set the attack timer to 1 second (FPS frames)
+            color_change_timer = int(0.1 * FPS)  # Set color change timer to 0.1 seconds
+
+        attack_timer = max(0, attack_timer - 1)  # Decrease the attack timer
+        color_change_timer = max(0, color_change_timer - 1)  # Decrease the color change timer
+
+        if color_change_timer > 0:
+            character_color = GREEN  # Character turns green for 0.1 seconds after being attacked
+        elif health <= 0:
+            character_color = PINK  # Character turns pink when health reaches 0
+        else:
+            character_color = RED  # Character remains red if not attacked
+
+        if distance > 0:
             dx /= distance
             dy /= distance
 
-            # Move the character in the direction
-            character.x += dx * CHARACTER_SPEED
-            character.y += dy * CHARACTER_SPEED
+            enemy.x += dx * ENEMY_SPEED
+            enemy.y += dy * ENEMY_SPEED
 
-        # Clear the screen
         screen.fill(WHITE)
 
-        # Draw the character
-        pygame.draw.rect(screen, (0, 0, 255), character)
+        pygame.draw.rect(screen, character_color, character)
+        pygame.draw.rect(screen, BLUE, enemy)
 
-        # Update the display
         pygame.display.flip()
 
-    # Quit Pygame
+        clock.tick(FPS) 
+
     pygame.quit()
