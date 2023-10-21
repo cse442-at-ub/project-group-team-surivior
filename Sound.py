792887f916d7a6694dee4c87e224e79b62f042f1
leoli33 <1641481115@qq.com>
2023-10-21 01:14:47 -0400

    add play_sound_effect function to proccess the sound effect in game.

--- Sound.py
+++ Sound.py
@@ -3,3 +3,7 @@ import pygame
 def play_music(text):
     pygame.mixer.music.load(text)
     pygame.mixer.music.play()
+
+def play_sound_effect(text):
+    effect = pygame.mixer.Sound(text)
+    effect.play
