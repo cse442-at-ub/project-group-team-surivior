19f78b8a4c522a3fb51d4c36fa09765aba5eec10
yluoc <98978059+yluoc@users.noreply.github.com>
2023-10-20 22:26:43 -0400

    items update (some deletion)

--- items.py
+++ items.py
@@ -10,6 +10,7 @@ shoes = {}
 shoes["Berserker's Greaves"] = {'attack speed': 0.35, 'movement speed': 45}
 shoes['Ionian Boots of Lucidity'] = {'ability haste': 20, 'movement': 45}
 shoes["Sorcerer's Shoes"] = {'magic penetration': 18, 'movement': 45}
+shoes['Plated Steelcaps'] = {'armor': 20,'movement': 45}
 
 #start items
 start_items = {}
@@ -24,28 +25,26 @@ start_items['Sapphire Crystal'] = {'mana': 250}
 
 #Epic items
 epic_items = {}
-epic_items['Blighting Jewel'] = {'ability power': 25, 'magic penetration': 0.15}
 epic_items['Serrated Dirk'] = {'attack damage': 30, 'lethality': 10}
-epic_items['Warhammer'] = {'attack damage': 25, 'ability haste': 10}
 epic_items['Kindlegem'] = {'ability haste': 10, 'health': 200}
 epic_items['Vampiric Scepter'] = {'attack damage': 10, 'lifesteal': 0.07}
+
 epic_items['Fiendish Codex'] = {'ability power': 35, 'aility haste': 10}
 epic_items['Leeching Leer'] = {'ability power': 20, 'health': 250, 'omnivamp': 0.5}
 epic_items['Needlessly Large Rod'] = {'ability power': 60}
+
 epic_items['Last Whisper'] = {'attack damage': 20, 'armor penetration': 0.18}
 epic_items['Zeal'] = {'attack speed': 0.15, 'critical strike chance': 0.15, 'movement': 0.05}
 epic_items['B.F.Sword'] = {'attack damage': 40}
+
 epic_items['Chain Vest'] = {'armor': 40}
 epic_items["Giant's Belt"] = {'health': 350}
-epic_items['Glacial Buckler'] = {'ability haste': 10, 'mana': 250, 'armor': 20}
 epic_items['Negatron Cloak'] = {'magic resistance': 50}
-epic_items['Aegis of the Legion'] = {'ability haste': 10, 'armor': 30,'magic resistance': 30}
+
 
 #Legendary items
 legendary_items ={}
 legendary_items['Cosmic Drive'] = {'ability power': 100, 'ability haste': 30, 'movement': 0.05}
-legendary_items['Void Staff'] = {'ability power': 65, 'magic penetration': 0.4}
-
 
 ############################################################################################################################################
 
@@ -53,73 +52,47 @@ legendary_items['Void Staff'] = {'ability power': 65, 'magic penetration': 0.4}
 
 # beginner items, you can choose one of them before gmae's beginning, these will not be added in item bar.
 doran_series = {}
-doran_series["Doran's Blade"] = {'attack damage': 8, 'health': 80} #UNIQUE
+doran_series["Doran's Blade"] = {'attack damage': 8, 'health': 80, 'omnivamp': 0.025}
 doran_series["Doran's Shield"] = {'health': 80, 'health regen': 0.8} #UNIQUE
 doran_series["Doran's Ring"] = {'ability power': 15, 'health': 80} #UNIQUE
 
 # shoes with unique stuff
 unique_shoes = {}
 unique_shoes["Mercury's Treads"] = {'magic resistance': 25, 'movement': 45} #UNIQUE
-unique_shoes['Plated Steelcaps'] = {'armor': 20,'movement': 45} #UNIQUE
 
 # epic items with unique stuff
 unique_epic_items = {}
-unique_epic_items['Hexdrinker'] = {'attack damage': 25, 'magic resistance': 35}
 unique_epic_items['Lost Chapter'] = {'ability power': 40, 'ability haste': 10,'mana': 300}
 unique_epic_items['Phage'] = {'attack damage': 15, 'health': 200}
-unique_epic_items['Tiamat'] = {'attack damage': 25} #UNIQUE
-unique_epic_items['Sheen'] = {}#UNIQUE
 unique_epic_items['Hextexh Alternator'] = {'ability power': 25, 'health': 150}#UNIQUE
-unique_epic_items['Catalyst of Aeons'] = {'health': 300, 'mana': 300}#UNIQUE
 unique_epic_items['Hearthbound Axe'] = {'attack damage': 20, 'attack speed': 0.15}#UNIQUE
 unique_epic_items['Kircheis Shard'] = {'attack damage': 15}#UNIQUE
-unique_epic_items['Noonquiver'] = {'attack damage': 30, 'attak speed': 0.15}#UNIQUE
 unique_epic_items['Rageknife'] = {'attack speed': 0.25}#UNIQUE
 unique_epic_items['Recurve Bow'] = {'attack speed': 0.15}#UNIQUE
 unique_epic_items["Bami's Cinder"] = {'health': 300}#UNIQUE
-unique_epic_items['Bramble Vest'] = {'Armor': 30}#UNIQUE
 unique_epic_items["Spectre's Cowl"] = {'health': 250, 'magic resistance': 25}#UNIQUE
 unique_epic_items["Warden's Mail"] = {'armor': 40}#UNIQUE
-unique_epic_items['Winged Moonplate'] = {'health': 150}#UNIQUE
 
 # unqiue legendary items
 unique_legendary_items = {}
-unique_legendary_items['Maw of Malmortius'] = {'attack damage': 65, 'magic resistance': 50}#UNIQUE
-unique_legendary_items['Titanic Hydra'] = {'attack damage': 30, 'health': 500}#UNIQUE
-unique_legendary_items['Ravenous Hydra'] = {'attack damage': 65, 'ability haste': 25, 'lifesteal': 0.1}#UNIQUE
-unique_legendary_items["Death's Dance"] = {'attack damage': 55, 'ability haste': 15, 'armor': 45}#UNIQUE
-unique_legendary_items['Spear of Shojin'] = {'attack damage': 55, 'ability haste': 20, 'health': 500}#UNIQUE
-unique_legendary_items['Divine Sunderer'] = {'attack damage': 40, 'ability haste': 20, 'health': 300}#UNIQUE
 unique_legendary_items['Demonic Embrace'] = {'ability power': 75, 'health': 350}#UNIQUE
 unique_legendary_items['Horizon Focus'] = {'ability power': 100, 'ability haste': 15, 'health': 150}#UNIQUE
-unique_legendary_items['Lich Bane'] = {'ability power': 85, 'ability haste': 15, 'movement': 0.08}#UNIQUE
 unique_legendary_items["Rabadon's Deathcap"] = {'ability power': 120}#UNIQUE
-unique_legendary_items["Rylai's Crystal Scepter"] = {'ability power': 75, 'health': 400}#UNIQUE
-unique_legendary_items['Rod of Ages'] = {'ability power': 60, 'health': 400, 'mana':400}#UNIQUE
 unique_legendary_items['Hextech Gunblade'] = {'ability power': 80, 'attack damage': 40, 'omnivamp': 0.2}#UNIQUE
-unique_legendary_items["Luden's Tempest"] = {'ability power': 80, 'ability haste': 20, 'mana': 600, 'magic penetration': 6}#UNIQUE
 unique_legendary_items["Liandry's Anguish"] = {'ability power': 100, 'ability haste': 20, 'mana': 600}#UNIQUE
 unique_legendary_items['Bloodthirster'] = {'attack damage': 55, 'critical strike chance': 0.2, 'lifesteal': 0.18}#UNIQUE
-unique_legendary_items['kraken Slayer'] = {'attack damage': 40, 'attack speed': 0.3, 'critical strike chance': 0.2}#UNIQUE
 unique_legendary_items["Lord Dominik's Regards"] = {'attack damage': 35, 'critical strike chance': 0.2, 'armor penetration': 0.3}#UNIQUE
 unique_legendary_items["Nashor's Tooth"] = {'ability power': 100, 'ability haste': 15, 'attack speed': 0.5}#UNIQUE
-unique_legendary_items["Rapid Firecannon"] = {'attack damage': 30, 'attack speed': 0.15, 'critical strike chance': 0.2, 'movement': 0.07}#UNIQUE
 unique_legendary_items["Runaan's Hurricane"] = {'attack speed': 0.4, 'critical strike chance': 0.2, 'movement': 0.07}#UNIQUE
 unique_legendary_items['Statikk Shiv'] = {'attack damage': 50, 'critical strike chance': 0.2, 'attack speed': 0.3}#UNIQUE
 unique_legendary_items["Wit's End"] = {'attack damage': 40, 'attack speed': 0.4, 'magic resistance': 40}#UNIQUE
-unique_legendary_items['Navori Quickblades'] = {'attack damage': 65, 'ability haste': 15, 'critical strike chance': 0.2}#UNIQUE
-unique_legendary_items["Guinsoo's Rageblade"] = {'attack damage': 30, 'attack speed': 0.25, 'ability power': 30}#UNIQUE
 unique_legendary_items['Abyssal Mask'] = {'ability haste': 10, 'health': 300, 'magic resistance': 60}#UNIQUE
-unique_legendary_items["Dead Man's Plate"] = {'armor': 45, 'health': 300, 'movement': 0.05}#UNIQUE
 unique_legendary_items['Force of Nature'] = {'magic rresistance': 60, 'health': 400, 'movement': 0.05}#UNIQUE
-unique_legendary_items["Randuin's Omen"] = {'health': 400, 'armor': 60}#UNIQUE
 unique_legendary_items['Spirit Visage'] = {'ability haste': 10, 'health': 450, 'magic resistance': 60, 'health regen': 1}#UNIQUE / health regen: 100%
 unique_legendary_items["Sterak's Gage"] = {'health': 450}#UNIQUE
-unique_legendary_items['Thornmail'] = {'health': 350, 'armor': 70}#UNIQUE
 unique_legendary_items["Warmog's Armor"] = {'ability haste': 10, 'health': 800, 'health regen': 2}#UNIQUE / health regen: 200%
-unique_legendary_items['Frostfire Gauntlet'] = {'ability haste': 20, 'health': 450, 'magic resistance': 25, 'armor': 25}#UNIQUE
 unique_legendary_items["Jak'sSho, The Protean"] = {'ability haste': 20, 'health': 400, 'magic resistance': 30, 'armor': 30}#UNIQUE
-unique_legendary_items['Heartsteel'] = {'ability haste': 20, 'health': 800, 'health regen': 2}#UNIQUE / health regen: 200%
+
 
 # supplies after the player getting 3 legendary items
 supplies = {}
