3fb98fcbe9f6f7b40fedfee82f217d6a26604fd5
Qiaoying <112420361+qy333@users.noreply.github.com>
2023-11-10 12:57:16 -0500

    adjust the font and position of score, update score system

--- score.py
+++ score.py
@@ -4,12 +4,12 @@ class ScoreBoard():
     def __init__(self):
         self.score = globalVar.score
         self.text_color = (255, 255, 255)
-        self.font = pygame.font.SysFont('Helvetica', 48)
+        self.font = pygame.font.SysFont('Helvetica', 36)
     
     def showScore(self):
         str_score = "Score: " + str(self.score) # change int to str
         self.score_image = self.font.render(str_score, False, self.text_color) # turn str to Surface object
-        self.score_rect = self.score_image.get_rect(center=(1500, 50)) # 获取分数图形的坐标位置
+        self.score_rect = self.score_image.get_rect(center=(1500, 20)) # 获取分数图形的坐标位置
         x = self.score_rect.x
         y = self.score_rect.y
         x_win, y_win = globalVar.screen.get_size()
