abc58f5cc0bcf776ce9779fa297ee407861e3965
yluoc <98978059+yluoc@users.noreply.github.com>
2023-11-10 19:35:33 -0500

    delete items, change item storage data structure

--- items.py
+++ items.py
@@ -1,97 +1,151 @@
-#items
-ITEMS = {}
-#items with unique attributes
-UNIQUE_ITEMS = {}
-#currency
-ITEMS['gold'] = {'value': 1} # need add image
+class Node:
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+        self.next = None
 
-#shoes
-ITEMS["Berserker's Greaves"] = {'attack speed': 0.35, 'movement speed': 45}
-ITEMS['Ionian Boots of Lucidity'] = {'ability haste': 20, 'movement': 45}
-UNIQUE_ITEMS["Mercury's Treads"] = {'magic resistance': 25, 'movement': 45} #UNIQUE
-UNIQUE_ITEMS['Plated Steelcaps'] = {'armor': 20,'movement': 45} #UNIQUE
-ITEMS["Sorcerer's Shoes"] = {'magic penetration': 18, 'movement': 45}
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-#start items
-ITEMS['Amplifying Tome'] = {'ability power': 20}
-ITEMS['Agility Cloak'] = {'critical strike chance': 0.15}
-ITEMS['Cloth Armor'] = {'armor': 15}
-ITEMS['Magic Mantle'] = {'magic resistance': 25}
-ITEMS['Dagger'] = {'attack speed': 0.12}
-ITEMS['Long Sword'] = {'attack damage': 10}
-ITEMS['Ruby Crystal'] = {'health': 150}
-ITEMS['Sapphire Crystal'] = {'mana': 250}
+    def append(self, name, data):
+        new_node = Node(name, data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+###############################################################shoes###################################################################################################
+ITEMS = LinkedList()
+Boots = {'current_moving_speed': 25}
+Berserkers_Greaves = {'current_attack_speed': 0.35, 'current_moving_speed': 45}
+cd_Boots = {'current_ability_haste': 20, 'current_moving_speed': 45}
+Sorcerers_Shoes = {'current_magic_penetration': 18, 'current_moving_speed': 45}
+Plated_Steelcaps = {'current_armor': 20,'current_moving_speed': 45}
+Mercurys_Treads = {'current_magic_resistance': 25, 'current_moving_speed': 45}
 
-#Epic items
-ITEMS['Serrated Dirk'] = {'attack damage': 30, 'lethality': 10}
-UNIQUE_ITEMS['Tiamat'] = {'attack damage': 25} #UNIQUE
-ITEMS['Warhammer'] = {'attack damage': 25, 'ability haste': 10}
-ITEMS['Hexdrinker'] = {'attack damage': 25, 'magic resistance': 35}
-ITEMS['Phage'] = {'attack damage': 15, 'health': 200}
-UNIQUE_ITEMS['Sheen'] = {}#UNIQUE
-ITEMS['Kindlegem'] = {'ability haste': 10, 'health': 200}
-ITEMS['ampiric Scepter'] = {'attack damage': 10, 'lifesteal': 0.07}
-ITEMS['Fiendish Codex'] = {'ability power': 35, 'aility haste': 10}
-UNIQUE_ITEMS['Hextexh Alternator'] = {'ability power': 25, 'health': 150}#UNIQUE
-ITEMS['Lost Chapter'] = {'ability power': 40, 'ability haste': 10,'mana': 300}
-UNIQUE_ITEMS['Catalyst of Aeons'] = {'health': 300, 'mana': 300}#UNIQUE
-ITEMS['Leeching Leer'] = {'ability power': 20, 'health': 250, 'omnivamp': 0.5}
-ITEMS['Needlessly Large Rod'] = {'ability power': 60}
-UNIQUE_ITEMS['Hearthbound Axe'] = {'attack damage': 20, 'attack speed': 0.15}#UNIQUE
-UNIQUE_ITEMS['Kircheis Shard'] = {'attack damage': 15}#UNIQUE
-ITEMS['Last Whisper'] = {'attack damage': 20, 'armor penetration': 0.18}
-UNIQUE_ITEMS['Noonquiver'] = {'attack damage': 30, 'attak speed': 0.15}#UNIQUE
-UNIQUE_ITEMS['Rageknife'] = {'attack speed': 0.25}#UNIQUE
-UNIQUE_ITEMS['Recurve Bow'] = {'attack speed': 0.15}#UNIQUE
-ITEMS['Zeal'] = {'attack speed': 0.15, 'critical strike chance': 0.15, 'movement': 0.05}
-ITEMS['B.F.Sword'] = {'attack damage': 40}
-UNIQUE_ITEMS["Bami's Cinder"] = {'health': 300}#UNIQUE
-UNIQUE_ITEMS['Bramble Vest'] = {'Armor': 30}#UNIQUE
-ITEMS['Chain Vest'] = {'armor': 40}
-ITEMS["Giant's Belt"] = {'health': 350}
-ITEMS['Glacial Buckler'] = {'ability haste': 10, 'mana': 250, 'armor': 20}
-ITEMS['Negatron Cloak'] = {'magic resistance': 50}
-UNIQUE_ITEMS["Spectre's Cowl"] = {'health': 250, 'magic resistance': 25}#UNIQUE
-UNIQUE_ITEMS["Warden's Mail"] = {'armor': 40}#UNIQUE
-UNIQUE_ITEMS['Winged Moonplate'] = {'health': 150}#UNIQUE
-ITEMS['Aegis of the Legion'] = {'ability haste': 10, 'armor': 30,'magic resistance': 30}
+ITEMS.append('Boots', Boots)
+ITEMS.append('Berserkers_Greaves', Berserkers_Greaves)
+ITEMS.append('cd_Boots', cd_Boots)
+ITEMS.append('Sorcerers_Shoes', Sorcerers_Shoes)
+ITEMS.append('Plated_Steelcaps', Plated_Steelcaps)
+ITEMS.append('Mercurys_Treads', Mercurys_Treads)
+################################################################start items############################################################################################
+
+Amplifying_Tome = {'current_ability_power': 20}
+Cloth_Armor = {'current_armor': 15}
+Magic_Mantle = {'current_magic_resistance': 25}
+Dagger = {'current_attack_speed': 0.12}
+Long_Sword = {'current_physical_damage_number': 10}
+Ruby_Crystal = {'current_health': 150}
+Sapphire_Crystal = {'current_mana': 250}
+
+ITEMS.append('Amplifying_Tome', Amplifying_Tome)
+ITEMS.append('Cloth_Armor', Cloth_Armor)
+ITEMS.append('Magic_Mantle', Magic_Mantle)
+ITEMS.append('Dagger', Dagger)
+ITEMS.append('Long_Sword', Long_Sword)
+ITEMS.append('Ruby_Crystal', Long_Sword)
+ITEMS.append('Sapphire_Crystal', Sapphire_Crystal)
+################################################################epic items#############################################################################################
+
+Serrated_Dirk = {'current_physical_damage_number': 30, 'current_lethality': 10}
+Kindlegem = {'current_ability_haste': 10, 'current_health': 200}
+Vampiric_Scepter = {'current_physical_damage_number': 10, 'current_lifesteal': 0.07}
+Fiendish_Codex = {'current_ability_power': 35, 'aility haste': 10}
+Leeching_Leer = {'current_ability_power': 20, 'current_health': 250, 'current_omnivamp': 0.5}
+Last_Whisper = {'current_physical_damage_number': 20, 'current_armor_penetration': 0.18}
+Zeal = {'current_attack_speed': 0.15, 'current_moving_speed': 0.05}
+Recurve_Bow = {'current_attack_speed': 0.15, 'current_magical_damage_number': 15}
+Chain_Vest = {'current_armor': 40}
+Giants_Belt = {'current_health': 350}
+Negatron_Cloak = {'current_magic_resistance': 50}
+
+ITEMS.append('Serrated_Dirk', Serrated_Dirk)
+ITEMS.append('Kindlegem', Kindlegem)
+ITEMS.append('Vampiric_Scepter', Vampiric_Scepter)
+ITEMS.append('Fiendish_Codex', Fiendish_Codex)
+ITEMS.append('Leeching_Leer', Leeching_Leer)
+ITEMS.append('Last_Whisper', Last_Whisper)
+ITEMS.append('Zeal', Zeal)
+ITEMS.append('Recurve_Bow', Recurve_Bow)
+ITEMS.append('Chain_Vest', Chain_Vest)
+ITEMS.append('Giants_Belt', Giants_Belt)
+ITEMS.append('Negatron_Cloak', Negatron_Cloak)
+################################################################legendary items########################################################################################
 
 #Legendary items
-UNIQUE_ITEMS['Maw of Malmortius'] = {'attack damage': 65, 'magic resistance': 50}#UNIQUE
-UNIQUE_ITEMS['Titanic Hydra'] = {'attack damage': 30, 'health': 500}#UNIQUE
-UNIQUE_ITEMS['Ravenous Hydra'] = {'attack damage': 65, 'ability haste': 25, 'lifesteal': 0.1}#UNIQUE
-UNIQUE_ITEMS["Death's Dance"] = {'attack damage': 55, 'ability haste': 15, 'armor': 45}#UNIQUE
-UNIQUE_ITEMS['Spear of Shojin'] = {'attack damage': 55, 'ability haste': 20, 'health': 500}#UNIQUE
-UNIQUE_ITEMS['Divine Sunderer'] = {'attack damage': 40, 'ability haste': 20, 'health': 300}#UNIQUE
-ITEMS['Cosmic Drive'] = {'ability power': 100, 'ability haste': 30, 'movement': 0.05}
-UNIQUE_ITEMS['Demonic Embrace'] = {'ability power': 75, 'health': 350}#UNIQUE
-UNIQUE_ITEMS['Horizon Focus'] = {'ability power': 100, 'ability haste': 15, 'health': 150}#UNIQUE
-UNIQUE_ITEMS['Lich Bane'] = {'ability power': 85, 'ability haste': 15, 'movement': 0.08}#UNIQUE
-UNIQUE_ITEMS["Rabadon's Deathcap"] = {'ability power': 120}#UNIQUE
-UNIQUE_ITEMS["Rylai's Crystal Scepter"] = {'ability power': 75, 'health': 400}#UNIQUE
-ITEMS['Void Staff'] = {'ability power': 65, 'magic penetration': 0.4}
-UNIQUE_ITEMS['Rod of Ages'] = {'ability power': 60, 'health': 400, 'mana':400}#UNIQUE
-UNIQUE_ITEMS['Hextech Gunlade'] = {'ability power': 80, 'attack damage': 40, 'omnivamp': 0.2}#UNIQUE
-UNIQUE_ITEMS["Luden's Tempest"] = {'ability power': 80, 'ability haste': 20, 'mana': 600, 'magic penetration': 6}#UNIQUE
-UNIQUE_ITEMS["Liandry's Anguish"] = {'ability power': 100, 'ability haste': 20, 'mana': 600}#UNIQUE
-UNIQUE_ITEMS['Bloodthirster'] = {'attack damage': 55, 'critical strike chance': 0.2, 'lifesteal': 0.18}#UNIQUE
-UNIQUE_ITEMS['kraken Slayer'] = {'attack damage': 40, 'attack speed': 0.3, 'critical strike chance': 0.2}#UNIQUE
-UNIQUE_ITEMS["Lord Dominik's Regards"] = {'attack damage': 35, 'critical strike chance': 0.2, 'armor penetration': 0.3}#UNIQUE
-UNIQUE_ITEMS["Nashor's Tooth"] = {'ability power': 100, 'ability haste': 15, 'attack speed': 0.5}#UNIQUE
-UNIQUE_ITEMS["Rapid Firecannon"] = {'attack damage': 30, 'attack speed': 0.15, 'critical strike chance': 0.2, 'movement': 0.07}#UNIQUE
-UNIQUE_ITEMS["Runaan's Hurricane"] = {'attack speed': 0.4, 'critical strike chance': 0.2, 'movement': 0.07}#UNIQUE
-UNIQUE_ITEMS['Statikk Shiv'] = {'attack damage': 50, 'critical strike chance': 0.2, 'attack speed': 0.3}#UNIQUE
-UNIQUE_ITEMS["Wit's End"] = {'attack damage': 40, 'attack speed': 0.4, 'magic resistance': 40}#UNIQUE
-UNIQUE_ITEMS['Navori Quickblades'] = {'attack damage': 65, 'ability haste': 15, 'critical strike chance': 0.2}#UNIQUE
-UNIQUE_ITEMS["Guinsoo's Rageblade"] = {'attack damage': 30, 'attack speed': 0.25, 'ability power': 30}#UNIQUE
-UNIQUE_ITEMS['Abyssal Mask'] = {'ability haste': 10, 'health': 300, 'magic resistance': 60}#UNIQUE
-UNIQUE_ITEMS["Dead Man's Plate"] = {'armor': 45, 'health': 300, 'movement': 0.05}#UNIQUE
-UNIQUE_ITEMS['Force of Nature'] = {'magic rresistance': 60, 'health': 400, 'movement': 0.05}#UNIQUE
-UNIQUE_ITEMS["Randuin's Omen"] = {'health': 400, 'armor': 60}#UNIQUE
-UNIQUE_ITEMS['Spirit Visage'] = {'ability haste': 10, 'health': 450, 'magic resistance': 60, 'health regen': 1}#UNIQUE / health regen: 100%
-UNIQUE_ITEMS["Sterak's Gage"] = {'health': 450}#UNIQUE
-UNIQUE_ITEMS['Thornmail'] = {'health': 350, 'armor': 70}#UNIQUE
-UNIQUE_ITEMS["Warmog's Armor"] = {'ability haste': 10, 'health': 800, 'health regen': 2}#UNIQUE / health regen: 200%
-UNIQUE_ITEMS['Frostfire Gauntlet'] = {'ability haste': 20, 'health': 450, 'magic resistance': 25, 'armor': 25}#UNIQUE
-UNIQUE_ITEMS["Jak'sSho, The Protean"] = {'ability haste': 20, 'health': 400, 'magic resistance': 30, 'armor': 30}#UNIQUE
-UNIQUE_ITEMS['Heartsteel'] = {'ability haste': 20, 'health': 800, 'health regen': 2}#UNIQUE / health regen: 200%
+
+Cosmic_Drive= {'current_ability_power': 100, 'current_ability_haste': 30, 'current_moving_speed': 0.05}
+
+ITEMS.append('Cosmic_Drive', Cosmic_Drive)
+############################################################################################################################################
+
+# beginner items, you can choose one of them before gmae's beginning, these will not be added in item bar.
+doran_series = LinkedList()
+Dorans_Blade = {'current_physical_damage_number': 8, 'current_health': 80, 'current_omnivamp': 0.025}
+Dorans_Shield = {'current_health': 80, 'current_health_regen': 0.8} 
+Dorans_Ring = {'current_ability_power': 15, 'current_health': 80} 
+doran_series.append('Dorans_Blade', Dorans_Blade)
+doran_series.append('Dorans_Shield', Dorans_Shield)
+doran_series.append('Dorans_Ring', Dorans_Ring)
+
+
+# epic items
+
+Lost_Chapter = {'current_ability_power': 40, 'current_ability_haste': 10,'current_mana': 300}
+Phage = {'current_physical_damage_number': 15, 'current_health': 200}
+Hextexh_Alternator = {'current_ability_power': 25, 'current_health': 150}
+Hearthbound_Axe = {'current_physical_damage_number': 20, 'current_attack_speed': 0.15}
+Kircheis_Shard = {'current_physical_damage_number': 15}
+Rageknife = {'current_attack_speed': 0.25}
+Bamis_Cinder = {'current_health': 300}
+Spectres_Cowl = {'current_health': 250, 'current_magic_resistance': 25}
+Wardens_Mail = {'current_armor': 40}
+
+ITEMS.append('Lost_Chapter', Lost_Chapter)
+ITEMS.append('Phage', Phage)
+ITEMS.append('Hextexh_Alternator', Hextexh_Alternator)
+ITEMS.append('Hearthbound_Axe', Hearthbound_Axe)
+ITEMS.append('Kircheis_Shard', Kircheis_Shard)
+ITEMS.append('Rageknife', Rageknife)
+ITEMS.append('Bamis_Cinder', Bamis_Cinder)
+ITEMS.append('Spectres_Cowl', Spectres_Cowl)
+ITEMS.append('Wardens_Mail', Wardens_Mail)
+
+#legendary items
+Demonic_Embrace = {'current_ability_power': 75, 'current_health': 350}
+Horizon_Focus = {'current_ability_power': 100, 'current_ability_haste': 15, 'current_health': 150}
+Rabadons_Deathcap = {'current_ability_power': 120}
+Hextech_Gunblade = {'current_ability_power': 80, 'current_physical_damage_number': 40, 'current_omnivamp': 0.2}
+Liandrys_Anguish = {'current_ability_power': 100, 'current_ability_haste': 20, 'current_mana': 600}
+Bloodthirster = {'current_physical_damage_number': 55, 'current_lifesteal': 0.18}
+Lord_Dominiks_Regards = {'current_physical_damage_number': 35, 'current_armor_penetration': 0.3}
+Nashors_Tooth = {'current_ability_power': 100, 'current_ability_haste': 15, 'current_attack_speed': 0.5}
+Runaans_Hurricane = {'current_attack_speed': 0.4, 'current_magical_damage_number': 20, 'current_moving_speed': 0.07}
+Statikk_Shiv = {'current_physical_damage_number': 50,  'current_attack_speed': 0.3}
+Wits_End = {'current_physical_damage_number': 40, 'current_attack_speed': 0.4, 'current_magic_resistance': 40}
+Abyssal_Mask = {'current_ability_haste': 10, 'current_health': 300, 'current_magic_resistance': 60}
+Force_of_Nature = {'magic rresistance': 60, 'current_health': 400, 'current_moving_speed': 0.05}
+Spirit_Visage = {'current_ability_haste': 10, 'current_health': 450, 'current_magic_resistance': 60, 'current_health_regen': 1}
+Steraks_Gage = {'current_health': 450}
+Warmogs_Armor = {'current_ability_haste': 10, 'current_health': 800, 'current_health_regen': 2} 
+
+ITEMS.append('Demonic_Embrace', Demonic_Embrace)
+ITEMS.append('Horizon_Focus', Horizon_Focus)
+ITEMS.append('Rabadons_Deathcap', Rabadons_Deathcap)
+ITEMS.append('Hextech_Gunblade', Hextech_Gunblade)
+ITEMS.append('Liandrys_Anguish', Liandrys_Anguish)
+ITEMS.append('Bloodthirster', Bloodthirster)
+ITEMS.append('Lord_Dominiks_Regards', Lord_Dominiks_Regards)
+ITEMS.append('Nashors_Tooth', Nashors_Tooth)
+ITEMS.append('Runaans_Hurricane', Runaans_Hurricane)
+ITEMS.append('Statikk_Shiv', Statikk_Shiv)
+ITEMS.append('Wits_End', Wits_End)
+ITEMS.append('Abyssal_Mask', Abyssal_Mask)
+ITEMS.append('Force_of_Nature', Force_of_Nature)
+ITEMS.append('Spirit_Visage', Spirit_Visage)
+ITEMS.append('Steraks_Gage', Steraks_Gage)
+ITEMS.append('Warmogs_Armor', Warmogs_Armor)
