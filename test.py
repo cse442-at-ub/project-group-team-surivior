9da4dd56b1fbf81f932930a9ae55361277a15839
leoli33 <1641481115@qq.com>
2023-12-09 03:54:38 -0500

    update test

---
+++ test.py
@@ -0,0 +1,4 @@
+if 'Lethal_Tempo' in globals() and 'Conqueror' in globals() and 'Dark_Harvest' in globals() and 'Grasp_of_the_Undying' in globals():
+    print("Rune Keystone classes are available in goldVar.py")
+else:
+    print("Rune Keystone classes are not available in goldVar.py")
